391172d13df33bd77ea5f5f5a304627a0e82a12b
Ero2033 <erodriguez3m@gmail.com>
2022-10-29 09:18:25 -0500

    Primer commit subiendo las carpetas de prg_andro

---
+++ pantalla_ini/android/app/src/main/kotlin/com/example/pantalla_ini/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pantalla_ini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pantalla_ini/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(child: Text('')
+
+    ),
+    );}
+}
